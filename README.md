17e41079d6d87c54b166781f42b5b9df58686f08
spilin <lyoshakr@gmail.com>
2021-01-18 21:23:02 +0200

    add index and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # esp32-cam
 Firmware for esp32-cam with OV2640 and OV7670 support
+
+To upload to your esp32-cam:
+1. Clone the repo
+2. Rename `wifi_credentials.h.example` to `wifi_credentials.h`
+3. Modify wifi credentials in `wifi_credentials.h`.
+4. Follow instructions from this [Guide](https://randomnerdtutorials.com/program-upload-code-esp32-cam/)
+
+After reboot you can access:
+1. Index with links to 2 images
+2. Image local-ip/image.jpg
+3. Image with flash local-ip/flash-image.jpg
---
+++ esp32-cam.ino
@@ -0,0 +1,248 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-video-streaming-web-server-camera-home-assistant/
+
+  IMPORTANT!!!
+   - Select Board "AI Thinker ESP32-CAM"
+   - GPIO 0 must be connected to GND to upload a sketch
+   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h" //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include "esp_http_server.h"
+#include "wifi_credentials.h"
+
+
+// AI Thinker Model
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+httpd_handle_t jpg_httpd = NULL;
+
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+
+static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size_t len){
+    jpg_chunking_t *j = (jpg_chunking_t *)arg;
+    if(!index){
+        j->len = 0;
+    }
+    if(httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK){
+        return 0;
+    }
+    j->len += len;
+    return len;
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div id="container">
+    <h2>ESP32-CAM Photos</h2>
+    <p>
+      <a href="/image.jpg">Photo without flashlight</a>
+    </p>
+    <p>
+      <a href="/flash-image.jpg">Photo with flashligh</a>
+    </p>
+  </div>
+</body>
+</html>)rawliteral";
+
+esp_err_t index_httpd_handler(httpd_req_t *req){
+    esp_err_t res = ESP_OK;
+
+    res = httpd_resp_set_type(req, "text/html");
+
+    if(res == ESP_OK){
+      res = httpd_resp_send(req, index_html, strlen(index_html));
+    }
+    return res;
+}
+
+esp_err_t jpg_httpd_handler(httpd_req_t *req){
+    esp_err_t res = ESP_OK;
+    res = httpd_handler(req);
+    return res;
+}
+
+esp_err_t flash_jpg_httpd_handler(httpd_req_t *req){
+    esp_err_t res = ESP_OK;
+    digitalWrite(4, HIGH);
+    delay(1000);
+    res = httpd_handler(req);
+    digitalWrite(4, LOW);
+    return res;
+}
+
+esp_err_t httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t fb_len = 0;
+    int64_t fr_start = esp_timer_get_time();
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        ESP_LOGE(TAG, "Camera capture failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+    res = httpd_resp_set_type(req, "image/jpeg");
+    if(res == ESP_OK){
+        res = httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+    }
+
+    if(res == ESP_OK){
+        if(fb->format == PIXFORMAT_JPEG){
+            fb_len = fb->len;
+            res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+        } else {
+            jpg_chunking_t jchunk = {req, 0};
+            res = frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
+            httpd_resp_send_chunk(req, NULL, 0);
+            fb_len = jchunk.len;
+        }
+    }
+    esp_camera_fb_return(fb);
+    int64_t fr_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "JPG: %uKB %ums", (uint32_t)(fb_len/1024), (uint32_t)((fr_end - fr_start)/1000));
+    return res;
+}
+
+void startCameraServer(){
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+
+  httpd_uri_t image_uri = {
+    .uri       = "/image.jpg",
+    .method    = HTTP_GET,
+    .handler   = jpg_httpd_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t flash_image_uri = {
+    .uri       = "/flash-image.jpg",
+    .method    = HTTP_GET,
+    .handler   = flash_jpg_httpd_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = index_httpd_handler,
+    .user_ctx  = NULL
+  };
+
+
+  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  if (httpd_start(&jpg_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(jpg_httpd, &image_uri);
+    httpd_register_uri_handler(jpg_httpd, &flash_image_uri);
+    httpd_register_uri_handler(jpg_httpd, &index_uri);
+  }
+}
+
+void setup() {
+  int wifi_connection_attempt = 0;
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  pinMode(4, OUTPUT); //Init flashligh
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  // Wi-Fi connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    wifi_connection_attempt++;
+    if (wifi_connection_attempt >= 10) {
+      ESP.restart();
+    }
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  Serial.print("Camera Stream Ready! Go to: http://");
+  Serial.print(WiFi.localIP());
+
+  // Start streaming web server
+  startCameraServer();
+}
+
+void loop() {
+  delay(1);
+}
